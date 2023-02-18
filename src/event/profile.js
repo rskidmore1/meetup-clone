be74cfbab669dd0e2418fe7218bb4fac2d8a6644
Ubuntu <ubuntu@ip-172-31-21-223.us-west-2.compute.internal>
2023-02-18 00:47:07 +0000

    Basic layout

--- src/event/profile.js
+++ src/event/profile.js
@@ -12,7 +12,7 @@ function EventProfile(props) {
 
   return (
     <div>
-      <div className="h-36 w-full flex justify-items-start bg-white">
+      <div className="h-36 mb-10 w-full flex justify-items-start bg-white">
         <div>
           <div className='px-2'>
             <p className="text-header font-bold">
@@ -31,26 +31,58 @@ function EventProfile(props) {
         </div>
       </div>
       <div className="flex flex-row justify-between">
-        <div>
+        <div className='w-2/3'>
           <div>
             <img src={require('./event-image.png')} />
           </div>
           <h2 className='float-left font-bold p-5 '>Details</h2>
           <p className='float-left w-full'>{detailsParagraph}</p>
         </div>
-        <div>
-          <div>
-            Organizer tools
+        <div className='flex flex-col content-center gap-5 w-1/3'>
+          <div className='w-44 h-9 border-2 border-black bg-slate-50 rounded-md flex flex-row justify-center'>
+            <span>
+              Organizer tools
+            </span>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+            </svg>
+
           </div>
-          <div>
-            Group
+          <div className='w-44 h-32 bg-slate-50 rounded-md flex flex-row'>
+            <div>
+              <img src={require('./tech-in-oc.png')} />
+            </div>
+            <div>
+              <p>Tech in OC</p>
+              <p>Public group</p>
+              <p>Rating</p>
+            </div>
+            {/* TODO: Should we pass a group ID and to look up? */}
           </div>
-          <div>
-            Time
+          <div className='w-44 h-fit bg-slate-50 rounded-md flex flex-col'>
+            <div className='flex flex-row'>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+              </svg>
+              <span>
+                Monday, March 6, 2023 at 7:00pm to Monday, March 6, 2023 at 9:00pm PST
+              </span>
+            </div>
+            <div className='flex flex-row'>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+              </svg>
+              <div>
+                <p>Venue Name</p>
+                <p>Address</p>
+              </div>
+            </div>
+            <div>
+              Map
+            </div>
           </div>
-          <div>
-            Location
-          </div>
+
           <div>
             Event Chat
           </div>
