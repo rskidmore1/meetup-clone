ac6c4a56fbc9d9a13b8bcd7ddca48bba3fcbe6ef
Ryan Skidmore <ryan.skidmore@yahoo.com>
2023-02-21 04:18:45 +0000

    Linting

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    "react/react-in-jsx-scope": "off"
+  },
+};
