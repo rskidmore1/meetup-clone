312fe96a611edf9f1cb6ebbcdbfc42b84e105e4a
Ryan Skidmore <ryan.skidmore@yahoo.com>
2023-03-16 18:31:16 +0000

    Display reply comments

--- components/Comment/Comment.js
+++ components/Comment/Comment.js
@@ -12,6 +12,7 @@ async function saveComment(comment) {
 
   const url = 'http://34.210.145.64:8000/comment/save-comment';
   const data = comment;
+  console.log(comment);
 
   const response = await fetch(
     url, {
@@ -73,46 +74,80 @@ function Comment(props) {
     <div className='flex flex-col gap-2'>
       <h2 className='font-bold self-start'>Comments</h2>
       {comments?.map((comment, index) => (
+        comment.top_level_comment ? (
 
-        <div className='flex flex-row gap-1' key={index}>
-          <Image
-            src="/host.png"
-            alt="host"
-            className="self-start rounded-full"
-            width={56}
-            height={56}
-          />
-          <div className="w-full pb-4">
-            <div className="w-full h-fit bg-white px-3 rounded-md">
-              <div className="flex flex-row justify-between">
-                <div>{comment?.user?.name}</div>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
-                </svg>
+          <div className='flex flex-row gap-1' key={index}>
+            <Image
+              src="/host.png"
+              alt="host"
+              className="self-start rounded-full"
+              width={56}
+              height={56}
+            />
+            <div className="w-full pb-4">
+              <div className="w-full h-fit bg-white px-3 rounded-md">
+                <div className="flex flex-row justify-between">
+                  <div>{comment?.user?.name}</div>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+                  </svg>
+                </div>
+                <div className="flex justify-self-start">
+                  {comment?.text}
+                  <div className={console.log(comment)}></div>
+
+                </div>
               </div>
-              <div className="flex justify-self-start">
-                {comment?.text}
-                <div className={console.log(comment)}></div>
-              </div>
-            </div>
-            <div className="pl-3 ">
-              <button onClick={() => toggleInputButton(index)}>
-                Reply
-              </button>
-              <div className={toggleInput && toggleIndex === index ? '' : 'hidden'}>
-                <form onSubmit={(e) => handleSubmit(e, false)}>
-                  <input type="text" name="text" placeholder="Add a comment..." className="w-[400px] h-20" />
-                  <button type="submit">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
-                      <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-                    </svg>
-                  </button>
-                </form>
+              <div className="pl-3 ">
+                <button onClick={() => toggleInputButton(index)}>
+                  Reply
+                </button>
+                {/* <div className={console.log(comment?.id)}></div> */}
+                <div className={console.log(comments.filter(childComment => childComment?.parent_comment_id === comment?.id))}></div>
+                {comments.filter(childComment => childComment?.parent_comment_id === comment?.id)?.map((childCommentObject) => (
+                  <div className='flex flex-row gap-1' key={index}>
+                    <Image
+                      src="/host.png"
+                      alt="host"
+                      className="self-start rounded-full"
+                      width={56}
+                      height={56}
+                    />
+                    <div className="w-full pb-4">
+                      <div className="w-full h-fit bg-white px-3 rounded-md">
+                        <div className="flex flex-row justify-between">
+                          <div>{childCommentObject?.user?.name}</div>
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+                          </svg>
+                        </div>
+                        <div className="flex justify-self-start">
+                          {childCommentObject?.text}
+                          {/* <div className={console.log(comment)}></div> */}
+
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ))
+
+                }
+
+                {/* Need to make a funciton to get all the related comments and then map over them  */}
+                <div className={toggleInput && toggleIndex === index ? '' : 'hidden'}>
+                  <form onSubmit={(e) => handleSubmit(e, false, comment?.id)}>
+                    <input type="text" name="text" placeholder="Add a comment..." className="w-[400px] h-20" />
+                    <button type="submit">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+                      </svg>
+                    </button>
+                  </form>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-
+        ) : (<></>)
       ))}
       <div className="border-[1px] border-gray-700"></div>
       <div className="self-start flex flex-row gap-1">
