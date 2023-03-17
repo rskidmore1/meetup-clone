231fb238cb4d67d2a6a89795474ad0131116e27e
Ryan Skidmore <ryan.skidmore@yahoo.com>
2023-03-17 00:30:30 +0000

    About tab

--- pages/group/index.js
+++ pages/group/index.js
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 function Group() {
   return (
@@ -13,9 +15,40 @@ function Group() {
             height={200}
           />
         </div>
-        <h2 className='font-bold p-5 self-start'>Details</h2>
-        <p>Details</p>
-        {/* <Comment parentObjectId={parentObjectId} /> */}
+        <div>
+          <Tabs>
+            <TabList>
+              <Tab>About</Tab>
+              <Tab>Events</Tab>
+              <Tab>Members</Tab>
+              <Tab>Photos</Tab>
+              <Tab>Discussions</Tab>
+              <Tab>More</Tab>
+            </TabList>
+
+            <TabPanel>
+              <div>
+                <p>Details</p>
+              </div>
+            </TabPanel>
+            <TabPanel>
+              <p>Replace with components</p>
+            </TabPanel>
+            <TabPanel>
+              <p>Replace with components</p>
+            </TabPanel>
+            <TabPanel>
+              <p>Replace with components</p>
+            </TabPanel>
+            <TabPanel>
+              <p>Replace with components</p>
+            </TabPanel>
+            <TabPanel>
+              <p>Replace with components</p>
+            </TabPanel>
+          </Tabs>
+        </div>
+
       </div>
 
       <div className='flex flex-col content-center gap-5 w-1/3'>
@@ -58,7 +91,10 @@ function Group() {
               <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
             </svg>
             <div>9,742</div>
-            <div> Public group</div>
+            <div>Public group</div>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z" />
+            </svg>
           </div>
           <div className="flex flex-row">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
