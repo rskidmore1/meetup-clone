03136d4da0aeea5b0409ceface389211b99b1f05
Ryan Skidmore <ryan.skidmore@yahoo.com>
2023-04-03 23:56:06 +0000

    Box format

--- components/Group/Events.js
+++ components/Group/Events.js
@@ -20,7 +20,7 @@ function Events(props) {
 
   return (
     <div className="flex flex-row w-[1000px] gap-10">
-      <div className="flex flex-col w-1/4 bg-white">
+      <div className="flex flex-col w-1/4 bg-white rounded-lg">
         <span>
           Upcoming
         </span>
@@ -30,51 +30,43 @@ function Events(props) {
         <span>
           Draft
         </span>
-
       </div>
       <div className="flex flex-col gap-5 w-1/2">
         {/* Make into a map and card */}
         {events?.map((event) => (
-          <div className="flex flex-col  bg-white w-full">
-            <div className="flex flex-row">
-              <div>
-                <div>
-                  <div>
-                    {event?.start_time}
-                  </div>
-                  <div>
-                    {event?.title}
-                  </div>
-                  <div className="flex flex-row">
-                    <span>
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
-                      </svg>
-                    </span>
-                    <span>
-                      {event?.location}
-                    </span>
-                  </div>
-                  <div>
-                    {/* {event?.picture} */}
-                    {/* TODO: make picture */}
-                  </div>
-
-                </div>
-                <div>
-                  {event?.details_paragraph}
-                </div>
-                <div className="flex flex-row justify-between">
-                  <span>
-                    {event?.attendees.length} Attendees
-                  </span>
-                  <button type="button" className="px-2 py-4 border-[1px] border-blue-200 text-blue-200">
-                    Attend
-                  </button>
-                  {/* TODO: add going or not going after making user log-in */}
-                </div>
-              </div>
+          <div className="flex flex-col  bg-white w-full rounded-lg p-2">
+            <div>
+              {event?.start_time}
+            </div>
+            <div className="text-2xl">
+              {event?.title}
+            </div>
+            <div className="flex flex-row gap-2 mb-2">
+              <span>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-gray-400">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+                </svg>
+              </span>
+              <span>
+                {event?.location}
+              </span>
+            </div>
+            <div>
+              {/* {event?.picture} */}
+              {/* TODO: make picture */}
+            </div>
+            <div className="text-sm">
+              {event?.details_paragraph}
+            </div>
+            <div className="flex flex-row justify-between w-max-full text-sm">
+              <span className='self-end'>
+                {event?.attendees.length} Attendees
+              </span>
+              <button type="button" className="p-2 border-[1px] border-blue-200 text-blue-200">
+                Attend
+              </button>
+              {/* TODO: add going or not going after making user log-in */}
             </div>
           </div>
         ))}
