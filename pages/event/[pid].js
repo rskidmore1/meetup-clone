210ba59d3fe8252f51cbfe55c6ca7a0315261d7d
Ryan Skidmore <ryan.skidmore@yahoo.com>
2023-04-10 23:01:04 +0000

    Cleanup

--- pages/event/[pid].js
+++ pages/event/[pid].js
@@ -12,7 +12,6 @@ async function retrieveEvent(parentObjectId) {
 
 async function saveAttendee(userId, parentObjectId) {
   const url = 'http://35.86.78.63:8000/events/save-attendee';
-  // put route change here
 
   const data = { "userId": userId, "eventId": parentObjectId };
 
@@ -32,16 +31,6 @@ async function saveAttendee(userId, parentObjectId) {
 
 function Event() {
 
-  let title = 'Some Event';
-  let host = "Ryan";
-  let hostPhoto = "/src/img/host.png";
-  let photo = "./someline";
-  let location = "someaddress";
-  let detailsParagraph = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vitae erat eleifend, egestas lorem eu, vehicula nisl. Cras bibendum tellus eu purus accumsan, quis aliquet ipsum cursus. Sed nec iaculis urna, ut lobortis lacus. Vestibulum at sem bibendum, porta dolor vel, fermentum tortor. Nulla non aliquam arcu. Integer eget aliquet risus. Nullam non malesuada felis. Sed commodo hendrerit erat, et placerat felis vulputate vel. Suspendisse non porta lectus. Cras in neque gravida, lacinia ex ut, tempus est. Curabitur quis massa non ante porta lacinia. Nunc nec urna ex. Maecenas lorem nunc, finibus sit amet tincidunt sit amet, fringilla euismod dolor. Quisque risus diam, consequat eu posuere maximus, finibus ac nulla. Donec feugiat ante id est elementum, a maximus purus sodales. Nullam efficitur odio vel nisl tincidunt tristique. Maecenas vestibulum bibendum arcu, at rutrum sem hendrerit ut. In a facilisis lacus. Donec vitae venenatis enim, sed commodo nibh. Aenean at blandit est. In id faucibus elit. Phasellus lobortis, nunc nec auctor accumsan, orci odio tempor nibh, ac iaculis urna justo sit amet tellus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere ipsum sit amet ultrices gravida.';
-  let startTime = "sometime";
-  let endTime = "sometime";
-  let group = "somegroupID";
-
   const router = useRouter();
   const { pid } = router.query;
 
@@ -51,6 +40,7 @@ function Event() {
   const [modal, setModal] = useState(false);
 
   const userId = "64221158e1bbeb5fc205ed21"
+  // TODO: replace during auth ticket
 
   useEffect(() => {
     retrieveEvent(pid).then(
