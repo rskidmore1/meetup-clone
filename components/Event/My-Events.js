db094bfb0a09047949af51c17ff5472c574f3a26
Ryan Skidmore <ryan.skidmore@yahoo.com>
2023-04-11 21:59:38 +0000

    Styled cards

--- components/Event/My-Events.js
+++ components/Event/My-Events.js
@@ -49,25 +49,29 @@ export default function MyGroups() {
     <div className="flex flex-col px-10 py-5 border-b-[1px] border-b-gray-500 ">
       <h1 className="font-bold text-2xl self-center">Events This Week</h1>
 
-      <div className="flex flex-row justify-center gap-5 pt-5">
+      <div className="flex flex-row flex-wrap justify-center gap-5 pt-5">
         {eventsThisWeek?.map((event) => (
           <Link href={"/event/" + `${event._id.$oid}`}>
-            <div className="flex flex-row bg-white gap-5 p-3 rounded-lg">
-              hello
+            <div className="flex flex-row bg-white gap-5 p-3 rounded-lg w-fit h-fit">
               <Image
                 src={event.photo}
                 alt="Event photo"
                 width={150}
                 height={150}
               />
-              {/* <div className="flex flex-col">
+              <div className="flex flex-col">
                 <span>
-                  {group.name}
+                  {event.title}
                 </span>
+                <div className="flex flex-row gap-2">
+                  <div>{event.day}</div>
+                  <div>{event.start_time}</div>
+                </div>
                 <span>
-                  {group.location}
+                  {event.location}
                 </span>
-              </div> */}
+                <span>{event.group}</span>
+              </div>
             </div>
           </Link>
         ))}
