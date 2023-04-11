8fe613d193cde7ebc61ebb6a362fb35206e57a8d
Ryan Skidmore <ryan.skidmore@yahoo.com>
2023-04-11 07:57:46 -0700

    Update Events.js

--- components/Group/Events.js
+++ components/Group/Events.js
@@ -16,7 +16,6 @@ function Events(props) {
       result => result.json()).then(
         data => {
           setEvents(data.events);
-          console.log(data.events);
         }
       );
   }, []);
@@ -37,7 +36,6 @@ function Events(props) {
       <div className="flex flex-col gap-5 w-1/2">
         {events?.map((event, index) => (
           <Link href={"/event/" + `${event._id.$oid}`}>
-            <div className={console.log(event['_id'])}></div>
             <div className="flex flex-col  bg-white w-full rounded-lg p-2" key={index}>
               <div className="flex flex-row justify-between">
                 <div>
